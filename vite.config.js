b3bfab94531c904fe3a9c91e68fdf3f7a000c07f
AldiPradanahakim <aldi.pradan.hakim26@gmail.com>
2024-12-09 12:44:32 +0700

    push progres

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,24 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/home.css',
+                'resources/js/home.js',
+                'resources/css/login.css',
+                'resources/css/signup.css',
+                'resources/js/signup.js',
+                'resources/js/genre.js',
+                'resources/css/genre.css',
+                'resources/js/halamanutama.js',
+                'resources/css/halamanutama.css'
+            ],
             refresh: true,
         }),
     ],
+    build: {
+        rollupOptions: {
+            // Tambahkan modul eksternal di sini jika perlu
+            external: ['resources/js/landingpage/section.js'],
+        },
+    },
 });
